140d8efaf04f650bd41c80dd21a6c144410000b7
wescox <24943022+wescox@users.noreply.github.com>
2020-11-22 15:18:50 -0500

    updated readme and beginning of user interface

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# price_drop_scraper
+# Price Drop Scraper
+Allows user to input the URL of a product page from Amazon or Target.  App will scrape the page for the current price, and then continue to scrape twice a day after.  User will receive an email if the price drops lower than the original price.
+
+## Debugging SMTP server
+For testing purposes, the app does actually email.  It's setup to use Python's built-in SMTP server which just prints the message to the screen.  Copy the code below and paste and execute in a terminal shell. 
+
+### Mac & Linux terminal
+sudo python3 -m smtpd -c DebuggingServer -n localhost:1025
+
+### Windows command prompt
+python -m smtpd -c DebuggingServer -n localhost:1025
+
+## Requirements
+pip install requests-html
+pip install pyppdf
--- scraper.py
+++ scraper.py
@@ -6,45 +6,49 @@ import smtplib
 from requests_html import HTMLSession
 from pyppdf import patch_pyppeteer
 
-# send email
-sender = 'from@fromdomain.com'
-receivers = ['to@todomain.com']
 
-message = """From: From Person <from@fromdomain.com>
-To: To Person <to@todomain.com>
-Subject: SMTP e-mail test
+# notification email
+def send_mail():
+    sender = 'from@fromdomain.com'
+    receivers = ['to@todomain.com']
+    message = """From: From Person <from@fromdomain.com>
+    To: To Person <to@todomain.com>
+    Subject: SMTP e-mail test
 
-This is a test e-mail message.
+    This is a test e-mail message.
+    """
+    try:
+        smtpObj = smtplib.SMTP('localhost', 1025)
+        smtpObj.sendmail(sender, receivers, message)         
+        print("Successfully sent email")
+    except SMTPException:
+        print("Error: unable to send email")
+
+
+# web scraper
+def get_price(url):
+    s = HTMLSession()
+    content = s.get(url)
+    content.html.render(sleep=1)
+    if "target" in url:
+        price = content.html.xpath('/html/body/div[1]/div/div[5]/div/div[2]/div[2]/div[1]/div[1]/div[1]', first = True).text
+    if "amazon" in url:
+        price = content.html.xpath('//*[@id="priceblock_ourprice"]', first = True).text
+    return price
+
+
+# user interface
+url = input(
 """
 
-try:
-   smtpObj = smtplib.SMTP('localhost', 1025)
-   smtpObj.sendmail(sender, receivers, message)         
-   print("Successfully sent email")
-except SMTPException:
-   print("Error: unable to send email")
+**********
+quit = q
+**********
 
+What's the URL of the product you want to track on Target?
+"""
+)
+print("\n\n" +  get_price(url) + "\n")
 
-url = "https://www.target.com/p/delta-children-adley-3-in-1-convertible-crib/-/A-53618889?preselect=75003678#lnk=sametab"
-s = HTMLSession()
-content = s.get(url)
-content.html.render(sleep=1)
-price = content.html.xpath('/html/body/div[1]/div/div[5]/div/div[2]/div[2]/div[1]/div[1]/div[1]', first = True).text
-
-print(price)
-
-url = "https://www.amazon.com/eufy-Security-Monitor-Display-Wide-Angle-dp-B07YD8W5B9/dp/B07YD8W5B9/ref=dp_ob_title_baby"
-s = HTMLSession()
-content = s.get(url)
-content.html.render(sleep=1)
-price = content.html.xpath('//*[@id="priceblock_ourprice"]', first = True).text
-
-print(price)
-
-#headers  = {
-#    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:82.0) Gecko/20100101 Firefox/82.0"
-#}
-#page = requests.get(URL, headers=headers)
-#content = BeautifulSoup(page.content, 'html.parser')
-#price = content.find(id="priceblock_ourprice")
-#print(content)
+#https://www.target.com/p/delta-children-adley-3-in-1-convertible-crib/-/A-53618889?preselect=75003678#lnk=sametab
+#https://www.amazon.com/eufy-Security-Monitor-Display-Wide-Angle-dp-B07YD8W5B9/dp/B07YD8W5B9/ref=dp_ob_title_baby
