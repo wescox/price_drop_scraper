119872cdf4bea60de083e52f73ac373a59a85f8e
wescox <24943022+wescox@users.noreply.github.com>
2020-11-27 17:30:33 -0500

    added beautifulsoup and additional retailer

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Allows user to input the URL of a product page from Amazon or Target.  App will scrape the page for the current price, and then continue to scrape twice a day after.  User will receive an email if the price drops lower than the original price.
 
 ## Debugging SMTP server
-For testing purposes, the app does actually email.  It's setup to use Python's built-in SMTP server which just prints the message to the screen.  Copy the code below and paste and execute in a terminal shell. 
+For testing purposes, the app does not actually email.  It's setup to use Python's built-in SMTP server which just prints the message to the screen.  Copy the code below and paste and execute in a terminal shell.  If you do not, the app still works and scrapes, but there will be an error and no test emails will be sent.
 
 ### Mac & Linux terminal
 sudo python3 -m smtpd -c DebuggingServer -n localhost:1025
@@ -10,6 +10,22 @@ sudo python3 -m smtpd -c DebuggingServer -n localhost:1025
 ### Windows command prompt
 python -m smtpd -c DebuggingServer -n localhost:1025
 
-## Requirements
-pip install requests-html
-pip install pyppdf
+## Dependencies
+pip install requests-html\
+pip install pyppdf\
+pip install bs4
+
+## Required Features List
+1. Implement a “master loop” console application where the user can repeatedly enter commands/perform actions, including choosing to exit the program
+    - The user interface meets this requirement
+2. Create a dictionary or list, populate it with several values, retrieve at least one value, and use it in your program
+    - Lists and dictionaries are used often.  The trackers.json file is a dictionary with lists of other dictionaries as values.  
+3. Read data from an external file, such as text, JSON, CSV, etc and use that data in your application
+    - Data is read from 2 different json files, one of which is created by the app. 
+4. Create and call at least 3 functions, at least one of which must return a value that is used
+    - There are many functions, all of which return a value used elsewhere.  
+5. Calculate and display data based on an external factor (ex: get the current date, and display how many days remaining until some event)
+    - App compares current external price to previously stored prices at certain times of day
+6. Implement a “scraper” that can be fed a type of file or URL and pull information off of it. For example, a web scraper that lets you provide any website URL and it will
+ find certain keywords on the page
+    -  App pulls title and price from product page URLs on Amazon, Target, and Best Buy websites.  
--- scraper.py
+++ scraper.py
@@ -1,10 +1,35 @@
 # must start a local debugging smtp server by executing the below command in a terminal shell
 # sudo python3 -m smtpd -c DebuggingServer -n localhost:1025
 
+#https://www.target.com/p/delta-children-adley-3-in-1-convertible-crib/-/A-53618889?preselect=75003678#lnk=sametab
+#https://www.amazon.com/eufy-Security-Monitor-Display-Wide-Angle-dp-B07YD8W5B9/dp/B07YD8W5B9/ref=dp_ob_title_baby
+
+# # print trackers to screen - add to interface
+# retailer_count = 0
+# tmp_list = []
+# for retailer in data:
+#     retailer_count += 1
+#     tmp_list.append(retailer)
+#     print(retailer_count, ': ', retailer.capitalize(), ':', sep= '')
+#     tracker_count = 0
+#     for key in data[retailer]:
+#         tracker_count += 1
+#         print('\t', tracker_count, ': ', key['title'], sep= '')
+#         print('\t  ', key['price'])
+
+# #del data[tmp_list[1]][2]
+
+# TODO: add regex for retailer in scrape
+# TODO: add header rotator to get_session
+
 # libraries
 import smtplib
+import json
+import time
+import random
 from requests_html import HTMLSession
-from pyppdf import patch_pyppeteer
+from pyppdf import patch_pyppeteer #only needed for Mac
+from bs4 import BeautifulSoup
 
 
 # notification email
@@ -21,34 +46,111 @@ def send_mail():
         smtpObj = smtplib.SMTP('localhost', 1025)
         smtpObj.sendmail(sender, receivers, message)         
         print("Successfully sent email")
-    except SMTPException:
+    except:
         print("Error: unable to send email")
 
 
+# open fake browser session
+def get_session(url):
+    if "target.com" in url:
+        s = HTMLSession()
+        page = s.get(url)
+        page.html.render(sleep=1)
+    else:
+        with open('headers.json') as f:
+            data = json.load(f)
+        r = requests.Session()
+        request = r.get(url, headers = headers)
+        page = BeautifulSoup(response.content, 'html.parser')
+    return page
+
+        
 # web scraper
-def get_price(url):
-    s = HTMLSession()
-    content = s.get(url)
-    content.html.render(sleep=1)
-    if "target" in url:
-        price = content.html.xpath('/html/body/div[1]/div/div[5]/div/div[2]/div[2]/div[1]/div[1]/div[1]', first = True).text
-    if "amazon" in url:
-        price = content.html.xpath('//*[@id="priceblock_ourprice"]', first = True).text
-    return price
+def scrape(url):
+    page = get_session(url)
+    dict = {'title': '', 'price': '', 'url': url}
+    if "target.com" in url:
+        price = page.html.xpath('/html/body/div[1]/div/div[5]/div/div[2]/div[2]/div[1]/div[1]/div[1]', first = True).text.strip()
+        dict['title'] = page.html.xpath('/html/body/div[1]/div/div[5]/div/div[1]/div[2]/h1/span', first = True).text.strip()
+        retailer = 'target'
+    elif "amazon.com" in url:
+        try:
+            price = page.find(id="priceblock_ourprice").get_text() # regular price
+        except:
+            price = page.find(id="priceblock_dealprice").get_text() # sale price
+        dict['title'] = page.find(id="priceblock_dealprice").get_text().strip()
+        retailer = 'amazon'
+    elif "bestbuy.com" in url:
+        price = soup.find(attrs = {'class':'priceView-hero-price priceView-customer-price'}).span.get_text()
+        retailer = 'best buy'
+    dict['price'] = float(price[1:])
+    update_json(dict, retailer)
+    print("{} tracker successfully added".format(retailer.capitalize()))
 
 
-# user interface
-url = input(
+# update json file
+def update_json(dict, retailer, file = 'tracker.json'):
+    with open(file) as f:
+        data = json.load(f)
+    tmp = data[retailer]
+    tmp.append(dict)
+    with open(file, 'w') as f:
+        json.dump(data, f, indent = 4)
+
+
+if __name__ == '__main__':
+    #initialize json file
+    retailers_dict = {'amazon':[], 'target':[], 'best buy': []}
+    try:
+        with open('tracker.json') as file:
+            pass
+    except:
+        with open('tracker.json', 'w') as file:
+            json.dump(retailers_dict, file, indent = 4)
+
+    # user interface
+    print(
 """
 
-**********
-quit = q
-**********
+Make sure to run the command below in a separate terminal window to initiate a debugging SMTP server:
+python3 -m smtpd -c DebuggingServer -n localhost:1025 (you'll need to use sudo on Mac or Linux)
 
-What's the URL of the product you want to track on Target?
+You can track products from these retailers:
+Amazon
+Target
+Best Buy
+
+When you're finished entering URLs, type 'q' and press return to exit. 
 """
-)
-print("\n\n" +  get_price(url) + "\n")
+    )
+    url = ''
+    while True:
+        url = input("\nWhat's the URL of the product you want to track?\n")
+        if url.lower() in ('q', 'quit', 'exit', 'stop'):
+            break
+        else:
+            print("\nProcessing...\n")
+            scrape(url)
 
-#https://www.target.com/p/delta-children-adley-3-in-1-convertible-crib/-/A-53618889?preselect=75003678#lnk=sametab
-#https://www.amazon.com/eufy-Security-Monitor-Display-Wide-Angle-dp-B07YD8W5B9/dp/B07YD8W5B9/ref=dp_ob_title_baby
+
+
+# # Create ordered dict from Headers above
+# # ordered_headers_list = []
+# # for headers in headers_list:
+# #     h = OrderedDict()
+# #     for header,value in headers.items():
+# #         h[header]=value
+# #     ordered_headers_list.append(h)
+
+# # for i in range(1,4):
+# #Pick a random browser headers
+# headers = headers_list[0]
+# #Create a request session
+# r = requests.Session()
+# r.headers = headers
+# response = r.get(url)
+# soup = BeautifulSoup(response.content, 'html.parser')
+# #html = soup.prettify("utf-8")
+# # with open("output2.html", "wb") as file:
+# #     file.write(html)
+# #price = soup.find(id="priceblock_ourprice").get_text()
